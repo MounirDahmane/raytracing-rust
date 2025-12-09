d3f4339610a458de40176e7bc8cfbaf4707b0226
Dahmane Mounir <dahmanemounirbaligh@gmail.com>
2025-12-09 23:02:59 +0100

    finishing Positionable Camera chapter

--- in_one_weekend/src/main.rs
+++ in_one_weekend/src/main.rs
@@ -17,6 +17,7 @@ use crate::sphere::Sphere;
 use crate::vec3::{Point3, Vec3};
 
 use std::rc::Rc;
+use std::f64::consts::PI;
 
 fn main() {
 
@@ -26,7 +27,7 @@ fn main() {
     let material_ground = Rc::new(lambertian::new(color::Color::new(0.8, 0.8, 0.0)));
     let material_center = Rc::new(lambertian::new(color::Color::new(0.1, 0.2, 0.5)));
     let material_left   = Rc::new(Dielectric::new(1.5));
-    let material_bubble           = Rc::new(Dielectric::new(1.0/1.5)); 
+    let material_bubble = Rc::new(Dielectric::new(1.0/1.5)); 
     let material_right       = Rc::new(Metal::new(color::Color::new(0.8, 0.6, 0.2), 1.0));
 
     world.add(Box::new(Sphere::new(Point3::new( 0.0, -100.5, -1.0), 100.0, material_ground)));
@@ -35,7 +36,7 @@ fn main() {
     world.add(Box::new(Sphere::new(Point3::new(-1.0,    0.0, -1.0),   0.4, material_bubble)));
     world.add(Box::new(Sphere::new(Point3::new( 1.0,    0.0, -1.0),   0.5, material_right)));
 
-    let mut cam = camera::Camera::init(16.0 / 9.0, 400, 100, 50);
+    let mut cam = camera::Camera::init(16.0 / 9.0, 400, 100, 50, 20.0);
     
     cam.render(&world);
 
